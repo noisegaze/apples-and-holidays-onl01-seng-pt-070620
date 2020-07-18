b0e2c99cea79c1a23088425a2b7c106c24a76196
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-18 20:16:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,7 +78,7 @@ def all_holidays_with_bbq(holiday_hash)
 holiday_hash.each do |season,holiday|
   binding.pry
     if season == :spring || season == :summer 
-      season each do |holiday,values|
+      season.each do |holiday,values|
         values.include?("BBQ")
       end 
     end   

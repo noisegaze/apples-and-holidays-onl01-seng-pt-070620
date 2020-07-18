fb3abae2b1e207b0de87ea8d71eeb806a5650503
Benjamin Stephanus <ben.stephanus@gmail.com>
2020-07-18 20:21:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,7 +75,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   collection =[]
-holiday_hash.each do |season,holiday|
+holiday_hash.each do |season,holidays|
     if season == :spring || season == :summer 
       season.each do |holiday,values|
      if   values.include?("BBQ") 
